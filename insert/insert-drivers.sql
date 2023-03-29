28f58fe449e785164ba3653906f8f8dced89a875
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-29 12:14:40 -0600

    Lines and drivers inserted already

---
+++ insert/insert-drivers.sql
@@ -0,0 +1,13 @@
+USE cdmx_subway;
+
+INSERT INTO drivers (DNI, `name`, last_name, birthday) VALUES
+("22972704M", "Felix", "Aguado", "1928-01-02"),
+("53260885S", "Manuela", "Fuentes", "1964-11-25"),
+("49072692F", "Jose Alberto", "Vergara", "1911-03-24"),
+("09296403X", "Joel", "Luna", "1982-10-06"),
+("22044948T", "Ahmed", "Collado", "1914-11-27"),
+("86103319J", "Blas", "Carballo", "1927-03-05"),
+("00720595M", "Francisco Jose", "Caceres", "1963-02-21"),
+("42668386M", "Maria Yolanda", "Borrego", "2003-09-15"),
+("87505156T", "Adam", "Soria", "1929-03-06"),
+("30662593M", "Inmaculada", "Montoya", "1998-08-30")
---
+++ insert/insert-lines.sql
@@ -0,0 +1,17 @@
+USE cdmx_subway;
+
+INSERT INTO `lines`
+(`number`, color, `name`)
+VALUES
+(1, "Rosa", "Línea 1"),
+(2, "Azul", "Línea 2"),
+(3, "Verde Olivo", "Línea 3"),
+(4, "Cian", "Línea 4"),
+(5, "Amarillo", "Línea 5"),
+(6, "Rojo", "Línea 6"),
+(7, "Naranja", "Línea 7"),
+(8, "Verde", "Línea 8"),
+(9, "Café", "Línea 9"),
+(10, "Morado", "Línea A"),
+(11, "Verde y Gris", "Línea B"),
+(12, "Oro", "Línea 12");
