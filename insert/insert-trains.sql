7a393a3ead9d499e7d73c152212164f4aa8eb5f5
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-29 12:51:41 -0600

    Stations inserted

---
+++ insert/insert-trains.sql
@@ -0,0 +1,13 @@
+USE cdmx_subway;
+
+INSERT INTO trains (serial_number, driver_id, `year`) VALUES
+('0JFxafE4q8', "22972704M", 1978),
+('saltair007', "53260885S", 1967),
+('and200the0', "49072692F", 1998),
+('rust66on98', "09296403X", 1989),
+('your13door', "22044948T", 1987),
+('0i87never9', "86103319J", 1970),
+('needed0990', "00720595M", 1963),
+('anything15', "42668386M", 1998),
+('013more890', "87505156T", 1989),
+('17whispers', "30662593M", 1977);
