9d29b2c018f7e6e3468986fe7d211712a9c6deb5
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-30 13:18:58 -0600

    Line id column added to stations rows

---
+++ update/update-line-id.sql
@@ -0,0 +1,242 @@
+USE cdmx_subway;
+
+-- UPDATE stations
+-- SET line_id = 2
+-- WHERE `name`
+-- IN (
+--     "Tasqueña",
+--     "General Anaya",
+--     "Ermita",
+--     "Portales",
+--     "Nativitas",
+--     "Villa de Cortés",
+--     "Xola",
+--     "Viaducto",
+--     "Chabacano",
+--     "San Antonio Abad",
+--     "Pino Suárez",
+--     "Zócalo",
+--     "Allende",
+--     "Bellas Artes",
+--     "Hidalgo",
+--     "Revolución",
+--     "San Cosme",
+--     "Normal",
+--     "Colegio Militar",
+--     "Popotla",
+--     "Cuitláhuac",
+--     "Tacuba",
+--     "Panteones",
+--     "Cuatro Caminos"
+-- );
+
+-- UPDATE stations
+-- SET line_id = 3
+-- WHERE `name`
+-- IN (
+--     "Universidad",
+--     "Copilco",
+--     "Miguel Ángel de Quevedo",
+--     "Viveros",
+--     "Coyoacán",
+--     "Zapata",
+--     "División Norte",
+--     "Eugenia",
+--     "Etiopía / Plaza de la Transpatencia",
+--     "Centro Médico",
+--     "Hospital General",
+--     "Niños heroes",
+--     "Balderas",
+--     "Juárez",
+--     "Hidalgo",
+--     "Guerrero",
+--     "Tlatelolco",
+--     "La Raza",
+--     "Potrero",
+--     "Deportivo 18 de Marzo",
+--     "Indios Verdes"
+-- );
+
+-- UPDATE stations
+-- SET line_id = 4
+-- WHERE `name`
+-- IN (
+--     "Santa Anita",
+--     "Jamaica",
+--     "Fray Servando",
+--     "Candelaria",
+--     "Morelos",
+--     "Canal del Norte",
+--     "Consulado",
+--     "Bondojito",
+--     "Talismán",
+--     "Martín Carrera"
+-- );
+
+-- UPDATE stations
+-- SET line_id = 5
+-- WHERE `name`
+-- IN (
+--     "Politécnico",
+--     "Instituto del Petróleo",
+--     "Autobuses del Norte",
+--     "La Raza",
+--     "Misterios",
+--     "Valle Gómez",
+--     "Consulado",
+--     "Eduardo Molina",
+--     "Aragón",
+--     "Oceanía",
+--     "Terminal Aérea",
+--     "Hangares",
+--     "Pantitlán"
+-- );
+
+-- UPDATE stations
+-- SET line_id = 6
+-- WHERE `name`
+-- IN (
+--     "Martín Carrera",
+--     "La Villa - Basílica",
+--     "Deportivo 18 de Marzo",
+--     "Lindavista",
+--     "Instituto del Petróleo",
+--     "Vallejo",
+--     "Norte 45",
+--     "Ferrería",
+--     "Azcapotzalco",
+--     "Tezozómoc",
+--     "El Rosario"
+-- );
+
+-- UPDATE stations
+-- SET line_id = 7
+-- WHERE `name`
+-- IN (
+--     "El Rosario",
+--     "Aquiles Serdán",
+--     "Camarones",
+--     "Refinería",
+--     "Tacuba",
+--     "San Joaquín",
+--     "Polanco",
+--     "Auditorio",
+--     "Constituyentes",
+--     "Tacubaya",
+--     "San Pedro de Los Pinos",
+--     "San Antonio",
+--     "Mixcoac",
+--     "Barranca del Muerto"
+-- );
+
+-- UPDATE stations
+-- SET line_id = 8
+-- WHERE `name`
+-- IN (
+--     "Constitución de 1917",
+--     "UAM-I",
+--     "Cerro de la Estrella",
+--     "Iztapalapa",
+--     "Atlalilco",
+--     "Escuadrón 201",
+--     "Aculco",
+--     "Apatlaco",
+--     "Iztacalco",
+--     "Coyuya",
+--     "Santa Anita",
+--     "La Viga",
+--     "Chabacano",
+--     "Obrera",
+--     "Doctores",
+--     "Salto del Agua",
+--     "San Juan de Letrán",
+--     "Bellas Artes",
+--     "Garibaldi"
+-- );
+
+-- UPDATE stations
+-- SET line_id = 9
+-- WHERE `name`
+-- IN (
+--     "Tacubaya",
+--     "Patriotismo",
+--     "Chilpancingo",
+--     "Centro Médico",
+--     "Lázaro Cárdenas",
+--     "Chabacano",
+--     "Jamaica",
+--     "Mixiuhca",
+--     "Velódromo",
+--     "Ciudad Deportiva",
+--     "Puebla",
+--     "Pantitlán"
+-- );
+
+-- UPDATE stations
+-- SET line_id = 10
+-- WHERE `name`
+-- IN (
+--     "La Paz",
+--     "Los Reyes",
+--     "Santa Marta",
+--     "Acatitla",
+--     "Peñon Viejo",
+--     "Guelatao",
+--     "Tepalcates",
+--     "Canal de San Juan",
+--     "Agrícola Oriental",
+--     "Pantitlán"
+-- );
+
+UPDATE stations
+SET line_id = 11
+WHERE `name`
+IN (
+    "Ciudad Azteca",
+    "Plaza Aragón",
+    "Olímpica",
+    "Ecatepec",
+    "Múzquiz",
+    "Río de los Remedios",
+    "Impulsora",
+    "Nezahualcóyotl",
+    "Villa de Aragón",
+    "Bosque de Aragón",
+    "Deportivo Oceanía",
+    "Oceanía",
+    "Romero Rubio",
+    "Flores Magón",
+    "San Lázaro",
+    "Morelos",
+    "Tepito",
+    "Lagunilla",
+    "Garibaldi",
+    "Guerrero",
+    "Buenavista"
+);
+
+UPDATE stations
+SET line_id = 12
+WHERE `name`
+IN (
+    "Tláhuac",
+    "Tlaltenco",
+    "Zapotitlán",
+    "Nopalera",
+    "Olivos",
+    "Tezonco",
+    "Periférico Ote",
+    "Calle 11",
+    "Lomas Estrella",
+    "San Andrés Tomatlán",
+    "Culhuacán",
+    "Atlalilco",
+    "Mexicaltzingo",
+    "Ermita",
+    "Eje central",
+    "Parque de los Venados",
+    "Zapata",
+    "Hospital 20 de Nov.",
+    "Insurgentes Sur",
+    "Mixcoac"
+);
