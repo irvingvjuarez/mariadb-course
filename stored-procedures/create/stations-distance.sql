63d6aca157772c188ce880fa8be6a795150dfadc
Irving Juárez <irvingjuarez274@gmail.com>
2023-04-06 11:55:49 -0600

    [CREATE] first two stored procedures created

--- stored-procedures/create/stations-distance.sql
+++ stored-procedures/create/stations-distance.sql
@@ -3,14 +3,31 @@ USE cdmx_subway;
 DELIMITER //
 
 CREATE PROCEDURE stations_distance(
-    IN station_a POINT,
-    IN station_b POINT
+    IN station_a VARCHAR(50),
+    IN station_b VARCHAR(50),
+    IN kms BOOLEAN
 )
 BEGIN
     SELECT ST_Distance_Sphere(
-        get_station_location(station_a),
-        get_station_location(station_b)
-    ) AS stations_distance;
+        (
+            SELECT `location`
+            FROM locations
+            WHERE station_id = (
+                SELECT id
+                FROM stations
+                WHERE `name` = station_a
+            )
+        ),
+        (
+            SELECT `location`
+            FROM locations
+            WHERE station_id = (
+                SELECT id
+                FROM stations
+                WHERE `name` = station_b
+            )
+        )
+    ) / IF (kms, 1000, 1) AS stations_distance;
 END //
 
 DELIMITER ;
